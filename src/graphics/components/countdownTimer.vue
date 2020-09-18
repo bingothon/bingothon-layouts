b634b0d95658dbdae22e14c587faf1d114e10d70
Floha <floha2011@hotmail.de>
2020-09-18 02:44:06 +0200

    countdown style

--- src/graphics/components/countdownTimer.vue
+++ src/graphics/components/countdownTimer.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="CountdownTimer">
-        {{currentTime}}
-    </div>
+	<div class="CountdownContainer FlexContainer">
+   	  <div
+		  class="CountdownTimer FlexContainer"
+		  v-html="currentTime"
+	  >
+      </div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -12,13 +16,37 @@ import { store } from '../../browser-util/state';
 @Component({})
 export default class CountdownTimer extends Vue {
     get currentTime(): string {
-        return store.state.countdownTimer.time;
+        return this.splitStringToSpans(store.state.countdownTimer.time);
     }
+
+	splitStringToSpans(string) {
+		return string.replace(/\S/g, '<span>$&</span>');
+	}
 }
 </script>
 
 <style>
-    .CountdownTimer {
-        /** TODO */
-    }
+	.CountdownContainer {
+		flex-direction: column;
+		color: white;
+	}
+	.CountdownTimer {
+		font-weight: 500;
+		font-size: 100px;
+		transition: 1s;
+		align-content: center;
+		text-align: center;
+	}
+	/* Each character in the timer is in a span; setting width so the numbers appear monospaced. */
+	.CountdownTimer >>> span {
+		display: inline-block;
+		width: 0.6em;
+		margin: 0.01em;
+		text-align: center;
+		font-weight: bold;
+	}
+	.CountdownTimer >>> span:nth-of-type(3), .TimerBox >>> span:nth-of-type(6) {
+		width: 0.22em;
+		margin-top: -0.15em; /* Make the colon appear more towards the centre. */
+	}
 </style>
