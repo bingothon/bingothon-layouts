8f7c221e6c0bf3263f90772d23623c4783d41185
Floha <floha2011@hotmail.de>
2021-11-16 20:34:13 +0100

    grid?

--- src/graphics/components/tracker.vue
+++ src/graphics/components/tracker.vue
@@ -1,97 +1,78 @@
 <template>
     <div class="Tracker">
-        <table>
-            <tr>
-                <div :class="`Sword ${!game.sword ? 'greyed' : ''}`">
-                    <img :src="`${basePath}sword_${game.sword || 0}.png`">
-                </div>
-                <div :class="`Crystal ${!game.crystals ? 'greyed' : ''}`">
-                    <img :src="`${basePath}crystal.png`">
-                    {{ game.crystals || 0 }}
-                </div>
-                <div :class="`Beetle ${!game.beetle ? 'greyed' : ''}`">
-                    <img :src="`${basePath}beetle_${game.beetle || 0}.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Mitts ${!game.mitts ? 'greyed' : ''}`">
-                    <img :src="`${basePath}mitts_${game.mitts || 0}.png`">
-                </div>
-                <div :class="`Scale ${!game.mitts ? 'greyed' : ''}`">
-                    <img :src="`${basePath}scale.png`">
-                </div>
-                <div :class="`Earrings ${!game.earrings ? 'greyed' : ''}`">
-                    <img :src="`${basePath}earrings.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Bottles ${!game.bottles ? 'greyed' : ''}`">
-                    <img :src="`${basePath}bottle.png`">
-                    {{ game.bottles || 0 }}
-                </div>
-                <div :class="`Bugnet ${!game.bugnet ? 'greyed' : ''}`">
-                    <img :src="`${basePath}bugnet.png`">
-                </div>
-                <div :class="`Slingshot ${!game.slingshot ? 'greyed' : ''}`">
-                    <img :src="`${basePath}slingshot.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Bombs ${!game.bombs ? 'greyed' : ''}`">
-                    <img :src="`${basePath}bombs.png`">
-                </div>
-                <div :class="`Bellows ${!game.bellows ? 'greyed' : ''}`">
-                    <img :src="`${basePath}bellows.png`">
-                </div>
-                <div :class="`Whip ${!game.whip ? 'greyed' : ''}`">
-                    <img :src="`${basePath}whip.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Clawshots ${!game.clawshots ? 'greyed' : ''}`">
-                    <img :src="`${basePath}clawshots.png`">
-                </div>
-                <div :class="`Bow ${!game.bow ? 'greyed' : ''}`">
-                    <img :src="`${basePath}bow.png`">
-                </div>
-                <div :class="`Seachart ${!game.seachart ? 'greyed' : ''}`">
-                    <img :src="`${basePath}sea_chart.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`SpiralCharge ${!game.spiralcharge ? 'greyed' : ''}`">
-                    <img :src="`${basePath}spiral_charge.png`">
-                </div>
-                <div :class="`CavesKey ${!game.caveskey ? 'greyed' : ''}`">
-                    <img :src="`${basePath}caveskey.png`">
-                </div>
-                <div :class="`Letter ${!game.letter ? 'greyed' : ''}`">
-                    <img :src="`${basePath}letter.png`">
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Rattle ${!game.rattle ? 'greyed' : ''}`">
-                    <img :src="`${basePath}rattle.png`">
-                </div>
-                <div :class="`Cage ${!game.cage ? 'greyed' : ''}`">
-                    <img :src="`${basePath}cage.png`">
-                </div>
-                <div :class="`Pouch ${!game.pouch ? 'greyed' : ''}`">
-                    <img :src="`${basePath}pouch.png`">
-                    {{ game.pouch || 0 }}
-                </div>
-            </tr>
-            <tr>
-                <div :class="`Wallet`">
-                    <img :src="`${basePath}wallet_${game.wallet || 0}.png`">
-                </div>
-                <div :class="`SoT ${!game.sot ? 'greyed' : ''}`">
-                    <img :src="`${basePath}stone_of_trials.png`">
-                </div>
-            </tr>
-
-            {{ game }}
-        </table>
+        <div :class="`Sword ${!game.sword ? 'greyed' : ''}`">
+            <img :src="`${basePath}sword_${game.sword || 0}.png`">
+        </div>
+        <div :class="`Crystal ${!game.crystals ? 'greyed' : ''}`">
+            <img :src="`${basePath}crystal.png`">
+            {{ game.crystals || 0 }}
+        </div>
+        <div :class="`Beetle ${!game.beetle ? 'greyed' : ''}`">
+            <img :src="`${basePath}beetle_${game.beetle || 0}.png`">
+        </div>
+        <div :class="`Mitts ${!game.mitts ? 'greyed' : ''}`">
+            <img :src="`${basePath}mitts_${game.mitts || 0}.png`">
+        </div>
+        <div :class="`Scale ${!game.mitts ? 'greyed' : ''}`">
+            <img :src="`${basePath}scale.png`">
+        </div>
+        <div :class="`Earrings ${!game.earrings ? 'greyed' : ''}`">
+            <img :src="`${basePath}earrings.png`">
+        </div>
+        <div :class="`Bottles ${!game.bottles ? 'greyed' : ''}`">
+            <img :src="`${basePath}bottle.png`">
+            {{ game.bottles || 0 }}
+        </div>
+        <div :class="`Bugnet ${!game.bugnet ? 'greyed' : ''}`">
+            <img :src="`${basePath}bugnet.png`">
+        </div>
+        <div :class="`Slingshot ${!game.slingshot ? 'greyed' : ''}`">
+            <img :src="`${basePath}slingshot.png`">
+        </div>
+        <div :class="`Bombs ${!game.bombs ? 'greyed' : ''}`">
+            <img :src="`${basePath}bombs.png`">
+        </div>
+        <div :class="`Bellows ${!game.bellows ? 'greyed' : ''}`">
+            <img :src="`${basePath}bellows.png`">
+        </div>
+        <div :class="`Whip ${!game.whip ? 'greyed' : ''}`">
+            <img :src="`${basePath}whip.png`">
+        </div>
+        <div :class="`Clawshots ${!game.clawshots ? 'greyed' : ''}`">
+            <img :src="`${basePath}clawshots.png`">
+        </div>
+        <div :class="`Bow ${!game.bow ? 'greyed' : ''}`">
+            <img :src="`${basePath}bow.png`">
+        </div>
+        <div :class="`Seachart ${!game.seachart ? 'greyed' : ''}`">
+            <img :src="`${basePath}sea_chart.png`">
+        </div>
+        <div :class="`SpiralCharge ${!game.spiralcharge ? 'greyed' : ''}`">
+            <img :src="`${basePath}spiral_charge.png`">
+        </div>
+        <div :class="`CavesKey ${!game.caveskey ? 'greyed' : ''}`">
+            <img :src="`${basePath}caveskey.png`">
+        </div>
+        <div :class="`Letter ${!game.letter ? 'greyed' : ''}`">
+            <img :src="`${basePath}letter.png`">
+        </div>
+        <div :class="`Rattle ${!game.rattle ? 'greyed' : ''}`">
+            <img :src="`${basePath}rattle.png`">
+        </div>
+        <div :class="`Cage ${!game.cage ? 'greyed' : ''}`">
+            <img :src="`${basePath}cage.png`">
+        </div>
+        <div :class="`Pouch ${!game.pouch ? 'greyed' : ''}`">
+            <img :src="`${basePath}pouch.png`">
+            {{ game.pouch || 0 }}
+        </div>
+        <div :class="`Wallet`">
+            <img :src="`${basePath}wallet_${game.wallet || 0}.png`">
+        </div>
+        <div :class="`SoT ${!game.sot ? 'greyed' : ''}`">
+            <img :src="`${basePath}stone_of_trials.png`">
+        </div>
+        {{ game }}
     </div>
 </template>
 
@@ -124,7 +105,7 @@ export default class Tracker extends Vue {
 <style scoped>
 .Tracker {
     display: grid;
-    grid-template-columns: auto auto auto;
+    grid-template-columns: 30px 30px 30px;
     /*background-color: black;*/
 }
 
@@ -133,18 +114,18 @@ export default class Tracker extends Vue {
 }
 
 .Crystal {
-    grid-row-start: 1;
-    grid-column-start: 2;
+    grid-row: 1 / span 1;
+    grid-column: 2 / span 1;
 }
 
 .Sword {
-    grid-row-start: 1;
-    grid-row-end: 2;
-    grid-column-start: 1;
+    grid-row: 1 / span 2;
+    grid-column: 1 / span 1;
 }
 
 .Beetle {
-    grid-column-start: 3;
+    grid-column: 3 / span 1;
+    grid-row: 1 / span 1;
 }
 
 /* Number on Item */
@@ -152,6 +133,10 @@ export default class Tracker extends Vue {
     align-content: center;
     text-align: center;
     font-size: 30pt;
+}
+
+img {
+    width: 30px;
 }
 
 </style>
