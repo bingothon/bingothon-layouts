b73025d12e44e1cc144ff2a08030f5bad98324f5
Floha <floha2011@hotmail.de>
2022-06-06 18:38:39 +0200

    reformat files

--- src/graphics/game-layout/15_9-2p.vue
+++ src/graphics/game-layout/15_9-2p.vue
@@ -1,55 +1,53 @@
 <template>
-	<div id="GameLayout">
-		<twitch-player id="stream1" streamIndex="0"></twitch-player>
-		<twitch-player id="stream2" streamIndex="1"></twitch-player>
-    <div id="fillbar" class="flexContainer"></div>
-    <player-info id="pi1" playerIndex="0" height=60px></player-info>
-    <player-info id="pi2" playerIndex="1" height=60px></player-info>
-		<test-game-container id="game"></test-game-container>
-    <test-timer-container id="timer"></test-timer-container>
-    <bingo-board id="Bingo-board" fontSize="30px"></bingo-board>
+    <div id="GameLayout">
+        <twitch-player id="stream1" streamIndex="0"></twitch-player>
+        <twitch-player id="stream2" streamIndex="1"></twitch-player>
+        <div id="fillbar" class="flexContainer"></div>
+        <player-info id="pi1" playerIndex="0" height=60px></player-info>
+        <player-info id="pi2" playerIndex="1" height=60px></player-info>
+        <test-game-container id="game"></test-game-container>
+        <test-timer-container id="timer"></test-timer-container>
+        <bingo-board id="Bingo-board" fontSize="30px"></bingo-board>
         <div id="SponsorContainer">
             <rotating-logo logo-asset-type="wideSmallLogos"></rotating-logo>
         </div>
-    <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="114px"/>
-	</div>
+        <discord-voice-display id="discord-voice" iconHeight="40px" nameWidth="114px"/>
+    </div>
 </template>
 
 <script lang="ts">
-	import { Component, Vue, Watch, Prop } from "vue-property-decorator";
-	import { nodecg, NodeCG } from "../../browser-util/nodecg";
-	import { Bingoboard, BingosyncSocket, BingoboardMeta } from "../../../schemas";
-	import { store, getReplicant } from "../../browser-util/state";
-  import TestTimerContainer from "../components/timerContainer.vue";
-	import TestGameContainer from "../components/gameContainer.vue";
-  import BingoBoard from "../components/bingoboard.vue";
-  import PlayerInfo from "../components/playerInfo.vue";
-  import TeamInfo from "../components/teamInfo.vue";
-  import PlayerTeamContainer from "../components/playerTeamContainer.vue";
-  import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
-  import { RunDataPlayer, RunDataTeam } from "../../../speedcontrol-types";
-  import TwitchPlayer from "../components/twitchStreamPlaceholder.vue";
-  import RotatingLogo from "../components/sponsorLogoRotation.vue";
+import {Component, Vue} from "vue-property-decorator";
+import {store, getReplicant} from "../../browser-util/state";
+import TestTimerContainer from "../components/timerContainer.vue";
+import TestGameContainer from "../components/gameContainer.vue";
+import BingoBoard from "../components/bingoboard.vue";
+import PlayerInfo from "../components/playerInfo.vue";
+import TeamInfo from "../components/teamInfo.vue";
+import PlayerTeamContainer from "../components/playerTeamContainer.vue";
+import DiscordVoiceDisplay from "../components/discordVoiceDisplay.vue";
+import {RunDataTeam} from "../../../speedcontrol-types";
+import TwitchPlayer from "../components/twitchStreamPlaceholder.vue";
+import RotatingLogo from "../components/sponsorLogoRotation.vue";
 
-	@Component({
-		components: {
-			BingoBoard,
-      TestGameContainer,
-      PlayerInfo,
-      TeamInfo,
-      PlayerTeamContainer,
-      TestTimerContainer,
-      DiscordVoiceDisplay,
-			TwitchPlayer,
-            RotatingLogo,
-		}
-	})
-
-	export default class GameLayout extends Vue {
-    get teams(): RunDataTeam[] {
-      return store.state.runDataActiveRun.teams;
+@Component({
+    components: {
+        BingoBoard,
+        TestGameContainer,
+        PlayerInfo,
+        TeamInfo,
+        PlayerTeamContainer,
+        TestTimerContainer,
+        DiscordVoiceDisplay,
+        TwitchPlayer,
+        RotatingLogo,
     }
-	}
+})
+
+export default class GameLayout extends Vue {
+    get teams(): RunDataTeam[] {
+        return store.state.runDataActiveRun.teams;
+    }
+}
 </script>
 
 <style scoped>
@@ -62,23 +60,25 @@
     background-image: url("../../../static/bg-new.jpg");
 }
 
-  #stream1 {
+#stream1 {
     position: absolute;
     top: 0px;
     left: 0px;
     width: 960px;
     height: 576px;
     border: 2px var(--container-border-color) solid;
-  }
-  #stream2 {
+}
+
+#stream2 {
     position: absolute;
     top: 0px;
     left: 960px;
     width: 960px;
     height: 576px;
     border: 2px var(--container-border-color) solid;
-  }
-  #fillbar {
+}
+
+#fillbar {
     position: absolute;
     top: 1000px;
     left: 0px;
@@ -86,54 +86,60 @@
     height: 80px;
     background-image: url("../../../static/middle-info-background.png");
     border: 2px var(--container-border-color) solid;
-  }
-  #discord-voice {
+}
+
+#discord-voice {
     position: absolute;
     top: 636px;
     left: 1280px;
     width: 250px;
     height: 364px;
-  }
-  #pi1 {
+}
+
+#pi1 {
     position: absolute;
     top: 576px;
     left: 0px;
     border: 2px var(--container-border-color) solid;
     width: 643px;
     box-sizing: border-box;
-  }
-  #pi2 {
+}
+
+#pi2 {
     position: absolute;
     top: 576px;
     left: 1281px;
     border: 2px var(--container-border-color) solid;
     width: 643px;
     box-sizing: border-box;
-  }
-  #Bingo-board {
+}
+
+#Bingo-board {
     position: absolute;
     top: 576px;
     left: 640px;
     border: 2px var(--container-border-color) solid;
     width: 640px;
     height: 424px;
-  }
-  #game {
+}
+
+#game {
     position: absolute;
     top: 818px;
     left: 0px;
     width: 640px;
     border: 2px var(--container-border-color) solid;
     height: 182px;
-  }
-  #timer{
+}
+
+#timer {
     position: absolute;
     top: 636px;
     left: 1530px;
     width: 390px;
     border: 2px var(--container-border-color) solid;
     height: 364px;
-  }
+}
 
 #SponsorContainer {
     position: absolute;
