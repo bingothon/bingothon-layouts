95bd6a83df723b9c19f0ee3b07a06c785a37a30e
lepelog <25211966+lepelog@users.noreply.github.com>
2022-06-13 22:16:03 +0200

    refactor omnibar msg control a bit

--- src/dashboard/omnibarMessage-control/main.vue
+++ src/dashboard/omnibarMessage-control/main.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-        <div v-for="(message, i) in omniBarMessages"
+        <div v-for="(message, i) in messages"
              :key="i"
         >
             <v-text-field
@@ -8,6 +8,16 @@
                 @keyup.enter="updateMessage(i)"
             >
                 <template v-slot:append-outer>
+                    <v-btn
+                        :disabled="messages[i].message===(omniBarMessages[i] || {}).message"
+                        dark
+                        small
+                        @click="updateMessage(i)"
+                    >
+                        <v-icon>
+                            mdi-content-save
+                        </v-icon>
+                    </v-btn>
                     <v-btn
                         dark
                         small
@@ -20,24 +30,33 @@
                 </template>
             </v-text-field>
         </div>
-        <v-btn
-            dark
-            small
-            @click="addMessage()"
+        <v-text-field
+            v-model="newMessage"
+            @keyup.enter="addMessage()"
         >
-            <v-icon>
-                mdi-plus-circle
-            </v-icon>
-        </v-btn>
+            <template v-slot:append-outer>
+                <v-btn
+                    :disabled="!newMessage"
+                    dark
+                    small
+                    @click="addMessage()"
+                >
+                    <v-icon>
+                        mdi-plus-circle
+                    </v-icon>
+                </v-btn>
+            </template>
+        </v-text-field>
     </v-app>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+import {Component, Vue, Watch} from 'vue-property-decorator';
 import {
     OmnibarMessages,
 } from '../../../schemas';
 import {store} from "../../browser-util/state";
+import clone from "clone";
 
 const bundleName = 'bingothon-layouts';
 
@@ -45,19 +64,27 @@ const bundleName = 'bingothon-layouts';
 export default class OBSControl extends Vue {
 
     messages: OmnibarMessages = [];
+    newMessage: string = "";
 
     get omniBarMessages(): OmnibarMessages {
-        this.messages = store.state.omnibarMessages;
         return store.state.omnibarMessages;
     }
 
+    @Watch('omniBarMessages', {immediate: true})
+    updateMsgsFromReplicant() {
+        this.messages = clone(this.omniBarMessages);
+    }
+
     addMessage(): void {
-        this.messages.push({message: ""});
-        nodecg.Replicant('omnibarMessages').value = this.messages;
+        if (this.newMessage) {
+            this.messages.push({message: this.newMessage});
+            this.newMessage = "";
+            nodecg.Replicant('omnibarMessages').value = this.messages;
+        }
     }
 
     deleteMessage(i: number) {
-        this.messages = this.messages.splice(i, 1);
+        this.messages = this.messages.filter((_, idx) => i !== idx);
         nodecg.Replicant('omnibarMessages').value = this.messages;
     }
 
