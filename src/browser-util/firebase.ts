6777aead58374f1e9f148c29694a8c999d060676
Floha <floha2011@hotmail.de>
2023-05-05 21:14:35 +0200

    restore firebase code

--- src/browser-util/firebase.ts
+++ src/browser-util/firebase.ts
@@ -7,15 +7,17 @@ const config = nodecg.bundleConfig as Configschema
 const firebaseConfig = config.firebaseConfig
 const logger = new nodecg.Logger(`${nodecg.bundleName}:firebase`)
 
-firebase.initializeApp(firebaseConfig)
+firebase
+    .initializeApp(firebaseConfig)
 
-// .auth()
-// .signInAnonymously()
-// .then(() => {
-//     logger.info('Signed in to Firebase')
-// })
-// .catch((error) => {
-//     logger.error('Failed to login to Firebase', error)
-// })
+    .auth()
+    .signInAnonymously()
+    .then(() => {
+        logger.info('Signed in to Firebase')
+    })
+    .catch((error) => {
+        logger.error('Failed to login to Firebase', error)
+    })
+
 logger.warn(`the loaded firebase database`, firebase.database())
 export const db = firebase.database()
