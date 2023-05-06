6fa2ce1454adce69bcfb47fda2d20f41f33ffdfa
Floha <floha2011@hotmail.de>
2023-05-06 15:33:26 +0200

    Change prettier config, include semicolons

--- src/browser-util/firebase.ts
+++ src/browser-util/firebase.ts
@@ -1,23 +1,23 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, signInAnonymously } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
-import { Configschema } from '../../configschema'
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInAnonymously } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { Configschema } from '../../configschema';
 
-const config = nodecg.bundleConfig as Configschema
+const config = nodecg.bundleConfig as Configschema;
 
-const firebaseConfig = config.firebaseConfig
-const logger = new nodecg.Logger(`${nodecg.bundleName}:firebase`)
+const firebaseConfig = config.firebaseConfig;
+const logger = new nodecg.Logger(`${nodecg.bundleName}:firebase`);
 
-const fbApp = initializeApp(firebaseConfig)
-const auth = getAuth(fbApp)
+const fbApp = initializeApp(firebaseConfig);
+const auth = getAuth(fbApp);
 signInAnonymously(auth)
     .then(() => {
-        logger.info('Signed in to Firebase')
+        logger.info('Signed in to Firebase');
     })
     .catch((error) => {
-        logger.error('Failed to login to Firebase', error)
-    })
+        logger.error('Failed to login to Firebase', error);
+    });
 
-export const db = getDatabase(fbApp)
+export const db = getDatabase(fbApp);
 
-logger.warn(`the loaded firebase database`, db)
+logger.warn(`the loaded firebase database`, db);
