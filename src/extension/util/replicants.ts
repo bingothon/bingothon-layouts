4ad6a551f49f24439d6cf6c727aaca1f68c014cc
Flo Ha <floha2011@hotmail.de>
2024-05-08 15:44:02 +0200

    discord works

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -26,6 +26,7 @@ import type {
     TrackerPrizes,
     TwitchChatBotData,
     TwitchStreams,
+    VoiceActivity
 } from 'schemas';
 import type NodeCGTypes from '@nodecg/types';
 import { get as nodecg } from './nodecg-api-context';
@@ -37,73 +38,77 @@ import { get as nodecg } from './nodecg-api-context';
 
 export const boardMetaRep = nodecg().Replicant<BingoboardMeta>('bingoboardMeta') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<BingoboardMeta>;
 export const bingoboardModeRep = nodecg().Replicant<BingoboardMode>(
-    'bingoboardMode',
+    'bingoboardMode'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<BingoboardMode>;
 export const boardRep = nodecg().Replicant<Bingoboard>('bingoboard') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<Bingoboard>;
 export const socketRep = nodecg().Replicant<BingosyncSocket>('bingosyncSocket') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<BingosyncSocket>;
 export const explorationBoardRep = nodecg().Replicant<ExplorationBingoboard>(
-    'explorationBingoboard',
+    'explorationBingoboard'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ExplorationBingoboard>;
 export const externalBoardRep = nodecg().Replicant<ExternalBingoboard>(
-    'externalBingoboard',
+    'externalBingoboard'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ExternalBingoboard>;
 export const externalBingoboardMetaRep = nodecg().Replicant<ExternalBingoboardMeta>(
-    'externalBingoboardMeta',
+    'externalBingoboardMeta'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ExternalBingoboardMeta>;
 export const donationTotalReplicant = nodecg().Replicant<DonationTotal>(
-    'donationTotal',
+    'donationTotal'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<DonationTotal>;
 export const openBidsReplicant = nodecg().Replicant<TrackerOpenBids>(
-    'trackerOpenBids',
+    'trackerOpenBids'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<TrackerOpenBids>;
 export const donationsReplicant = nodecg().Replicant<TrackerDonations>(
-    'trackerDonations',
+    'trackerDonations'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<TrackerDonations>;
 export const prizesReplicant = nodecg().Replicant<TrackerPrizes>('trackerPrizes') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<TrackerPrizes>;
 export const allGameLayoutsRep = nodecg().Replicant<AllGameLayouts>(
-    'allGameLayouts',
+    'allGameLayouts'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<AllGameLayouts>;
 export const currentGameLayoutRep = nodecg().Replicant<CurrentGameLayout>(
-    'currentGameLayout',
+    'currentGameLayout'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<CurrentGameLayout>;
 export const songDataRep = nodecg().Replicant<SongData>('songData', { persistent: false }) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<SongData>;
 export const obsCurrentSceneRep = nodecg().Replicant<string | null>('obsCurrentScene');
 export const obsDashboardAudioSourcesRep = nodecg().Replicant<ObsDashboardAudioSources>(
-    'obsDashboardAudioSources',
+    'obsDashboardAudioSources'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsDashboardAudioSources>;
 export const obsAudioSourcesRep = nodecg().Replicant<ObsAudioSources>(
-    'obsAudioSources',
+    'obsAudioSources'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsAudioSources>;
 export const obsConnectionRep = nodecg().Replicant<ObsConnection>('obsConnection') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsConnection>;
 export const obsStreamModeRep = nodecg().Replicant<ObsStreamMode>('obsStreamMode') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsStreamMode>;
 export const discordDelayInfoRep = nodecg().Replicant<DiscordDelayInfo>(
-    'discordDelayInfo',
+    'discordDelayInfo'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<DiscordDelayInfo>;
 export const obsPreviewScene = nodecg().Replicant<string>('obsPreviewScene') as unknown as NodeCGTypes.ServerReplicant<string>;
 export const voiceDelayRep = nodecg().Replicant<number>('voiceDelay', {
     persistent: true,
-    defaultValue: 0,
+    defaultValue: 0
 }) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<number>;
 export const streamsReplicant = nodecg().Replicant<TwitchStreams>('twitchStreams', {
-    defaultValue: [],
+    defaultValue: []
 }) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<TwitchStreams>;
 export const soundOnTwitchStream = nodecg().Replicant<number>('soundOnTwitchStream', {
-    defaultValue: -1,
+    defaultValue: -1
 }) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<number>;
 export const hostDiscordDuringIntermissionRep = nodecg().Replicant<HostsSpeakingDuringIntermission>(
-    'hostsSpeakingDuringIntermission',
+    'hostsSpeakingDuringIntermission'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<HostsSpeakingDuringIntermission>;
 export const lastIntermissionTimestampRep = nodecg().Replicant<LastIntermissionTimestamp>(
-    'lastIntermissionTimestamp',
+    'lastIntermissionTimestamp'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<LastIntermissionTimestamp>;
 export const obsPreviewImgRep = nodecg().Replicant<ObsPreviewImg>('obsPreviewImg') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsPreviewImg>;
 export const twitchChatBotDataRep = nodecg().Replicant<TwitchChatBotData>(
-    'twitchChatBotData',
+    'twitchChatBotData'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<TwitchChatBotData>;
 export const obsSceneListRep = nodecg().Replicant<ObsSceneList>('obsSceneList') as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsSceneList>; // TODO: create a type Scene and replace 'any' with 'Scene[]'
 export const capturePositionsRep = nodecg().Replicant<CapturePositions>(
-    'capturePositions',
+    'capturePositions'
 ) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<CapturePositions>;
 export const obsAudioLevels = nodecg().Replicant<ObsAudioLevels>('obsAudioLevels', {
-    persistent: false,
+    persistent: false
 }) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<ObsAudioLevels>;
+export const voiceActivityRep = nodecg().Replicant<VoiceActivity>('voiceActivity', {
+    defaultValue: { members: [] },
+    persistent: true
+}) as unknown as NodeCGTypes.ServerReplicantWithSchemaDefault<VoiceActivity>;
