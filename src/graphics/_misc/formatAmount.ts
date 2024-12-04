2634b929169bf1f0c86b22bc35a33a93e178e4d3
lepelog <25211966+lepelog@users.noreply.github.com>
2024-12-04 22:43:27 +0100

    no space

--- src/graphics/_misc/formatAmount.ts
+++ src/graphics/_misc/formatAmount.ts
@@ -1,8 +1,8 @@
 const CURRENCY_SYMBOL = '$';
 export function formatAmount(amount: number, showCents = true) {
     if (showCents) {
-        return `${CURRENCY_SYMBOL} ${amount.toFixed(2)}`;
+        return `${CURRENCY_SYMBOL}${amount.toFixed(2)}`;
     } else {
-        return `${CURRENCY_SYMBOL} ${amount.toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
+        return `${CURRENCY_SYMBOL}${amount.toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
     }
 }
