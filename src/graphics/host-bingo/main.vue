78c0bb6b81d23904c5da44a46b07c51399598c80
Floha <floha2011@hotmail.de>
2025-12-05 15:28:52 +0100

    add separate host bingo graphic

---
+++ src/graphics/host-bingo/main.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="HostBingo">
+        <div id="HostingBingo">
+            <HostBingo dashboard="true" fontSize="25px"></HostBingo>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import HostBingo from '../components/hostBingo.vue';
+
+    @Component({
+        components: {
+            HostBingo
+        }
+    })
+    export default class HostingBingo extends Vue {}
+</script>
+
+<style>
+    body {
+        background-color: black;
+    }
+
+    button {
+        font-size: 15px;
+        text-align: center;
+        align-content: center;
+    }
+
+    #HostingBingo {
+        width: 100%;
+        margin-bottom: 10px;
+        padding-bottom: 80px;
+    }
+
+    #HostingBingo > .BingoBoard {
+        width: 100%;
+        padding-bottom: 100%;
+        position: relative;
+    }
+</style>
